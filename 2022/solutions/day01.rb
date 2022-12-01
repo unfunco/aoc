085e58f540ed5c350fe76e16be5a1fdef739ffe5
Daniel Morris <daniel@honestempire.com>
2022-12-01 17:57:57 +0000

    Fix printf placeholders

--- 2022/solutions/day01.rb
+++ 2022/solutions/day01.rb
@@ -1,12 +1,12 @@
 # Part 1: 69289
 # How many calories are being carried by the elf carrying the most calories?
-puts "Part 1: %i" % File.read("day01_input.txt").split(/\n{2,}/).map { |elf|
+puts "Part 1: %d" % File.read("day01_input.txt").split(/\n{2,}/).map { |elf|
   elf.split($/).map(&:to_i).sum
 }.max
 
 # Part 2: 205615
 # How many calories are being carried by the top 3 elves carrying the most
 # calories?
-puts "Part 2: %i" % File.read("day01_input.txt").split(/\n{2,}/).map { |elf|
+puts "Part 2: %d" % File.read("day01_input.txt").split(/\n{2,}/).map { |elf|
   elf.split($/).map(&:to_i).sum
 }.max(3).sum
